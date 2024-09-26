d4157566f3a93622e79a98a5a5a7dc53f85ee0a0
busrabektas <busraasbektas@gmail.com>
2024-09-26 12:02:32 +0300

    producer updated

--- book_service/app/producer.py
+++ book_service/app/producer.py
@@ -1,21 +1,3 @@
-# from confluent_kafka import Producer
-# import os
-
-# KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL", "kafka:9092")
-# KAFKA_TOPIC = "book_updates"
-
-# p = Producer({'bootstrap.servers': KAFKA_BROKER_URL})
-
-# def delivery_report(err, msg):
-#     """ Mesaj gönderim durumu. """
-#     if err is not None:
-#         print(f"Message delivery failed: {err}")
-#     else:
-#         print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
-
-# def send_book_update(book_data):
-#     p.produce(KAFKA_TOPIC, key="book_update", value=str(book_data), callback=delivery_report)
-#     p.flush()  # Tüm mesajların gönderilmesini bekler
 
 from kafka import KafkaProducer
 import json
